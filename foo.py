0ae35be2efdb6e474a4eb062e6300ed8b824b0a1
Howard Yeh <hayeah@gmail.com>
2023-05-02 03:31:02 +0000

    changes

--- foo.py
+++ foo.py
@@ -3,10 +3,26 @@ import requests
 import aiohttp
 
 import openai
+import json
+
+from pydantic import BaseModel
+
+
+class OpenaiConfig(BaseModel):
+    secret: str
+
+
+class AppConfig(BaseModel):
+    openai: OpenaiConfig
+
+
+# parse CONFIG_JSON into a dict
+
+config = AppConfig.parse_raw(os.environ.get("CONFIG_JSON", "{}"))
 
 if __name__ == "__main__":
-    print(requests)
-    help(openai)
-    print("hello world")
-    openai.api_key = os.environ["OPENAI_API_KEY"]
-
+    # print(requests)
+    # help(openai)
+    # print("hello world")
+    print(config.openai.secret)
+    # openai.api_key = os.environ["OPENAI_API_KEY"]
